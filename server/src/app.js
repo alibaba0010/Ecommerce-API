a9b72f2707cbbfae5f6070793dc1bdd64c76995e
alibaba0010 <zakariyahali100@gmail.com>
2023-03-12 15:12:36 +0100

    Working with middleware

--- server/src/app.js
+++ server/src/app.js
@@ -18,10 +18,18 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 const app = express();
 app
   .use(cors())
   .use(json())
+  .use(limiter)
   .get("/", (req, res) => res.send("Ecommerce-API Homepage"))
   .use("/products", express.static(path.join(__dirname, "./uploads")))
   .use("/v1", userRouter)

8915ad5f1ffd52a03debc4bad1754c65fba347f8
alibaba0010 <zakariyahali100@gmail.com>
2023-07-28 16:21:06 +0100

    merge issue

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -5,61 +5,87 @@ import notFoundError from "../errors/notFound.js";
 import UnAuthenticatedError from "../errors/unaunthenticated.js";
 import jwt from "jsonwebtoken";
 import { sendEmail } from "../services/Email.js";
+import {
+  checkAdmin,
+  checkEmailExists,
+  comparePassword,
+  findUser,
+  requiredFields,
+} from "../model/user/user.model.js";
 
-export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+export async function httpAddNewUser(req, res) {
+  const { username, email, password, confirmPassword } = req.body;
 
-  if ((!name, !email, !password))
-    // if ((!name|| !email ||!password))
-    throw new BadRequestError("Please fill all required field");
-  if (password.length < 6)
-    throw new BadRequestError("Password must be 6 or more characters");
-  const checkEmailExist = await User.findOne({ email });
-  if (checkEmailExist) throw new BadRequestError("Email already exists");
+  comparePassword(password, confirmPassword);
 
-  const user = await User.create({ name, email, password });
+  requiredFields(username, email, password, confirmPassword);
 
-  const token = await user.createJWT();
+  await checkEmailExists(email);
 
-  res.cookie("token", token, {
-    path: "/",
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 86400),
-    sameSite: "none",
-    secure: false,
-  });
-  if (user) {
-    const { name, id, email, image, contact, bio } = user;
-    res
-      .status(StatusCodes.CREATED)
-      .json({ name, email, id, token, image, contact, bio }); //_id: user.id
-  } else {
-    throw new BadRequestError("Invalid User data");
-  }
-};
+  const user = await User.create({ username, email, password });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ username: user.username, email: user.email, id: user._id });
+}
+// FOR ADMIN
+export async function httpAddNewAdmin(req, res) {
+  const admin = req.body;
+  admin.isAdmin = true;
+  const { username, email, password, confirmPassword, isAdmin } = admin;
 
-export const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  if ((!email, !password))
-    throw new BadRequestError("Please fill all required field");
-  const checkUsers = await User.findOne({ email });
+  comparePassword(password, confirmPassword);
+
+  requiredFields(username, email, password, confirmPassword);
+
+  const user = await User.create({ username, email, password, isAdmin });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ username: user.username, email: user.email, id: user._id });
+}
+// LOGIN
+export async function httpLogin(req, res) {
+  const { username, password } = req.body;
+  if (!username || !password)
+    throw new BadRequestError("Provide a username and password");
+  const checkUsers = await User.findOne({ username });
+
   if (!checkUsers) throw new UnAuthenticatedError("Invalid Credentials");
 
-  const checkPassword = await checkUsers.comparePassword(password);
-  if (!checkPassword) throw new UnAuthenticatedError("Invalid Password");
-
   const token = await checkUsers.createJWT();
-  res.cookie("token", token, {
-    path: "/",
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 86400),
-    sameSite: "none",
-    secure: false,
-  });
-  res.status(StatusCodes.OK).json({ email: checkUsers.email, token });
-});
 
-export const loginOutUser = asyncHandler(async (req, res) => {
+  res
+    .status(StatusCodes.CREATED)
+    .json({ username: user.username, email: user.email, id: user._id });
+}
+// GET ALL USERS
+export async function getAllUserByAdmin(req, res) {
+  const { skip, limit } = getPagination(req.query);
+  const users = await User.find({}, { __v: 0, password: 0 })
+    .sort("createdAt")
+    .skip(skip)
+    .limit(limit);
+
+  if (!users) throw new notFoundError("Unable to get Users");
+
+  return res.status(StatusCodes.OK).json({ users, nbHits: users.length });
+}
+
+// Get a user by admin
+export async function getUserByAdmin(req, res) {
+  const userId = req.params.id;
+  const user = await User.findById(userId);
+  if (!user) throw new notFoundError(`Unable to get User ${userId}`);
+  const { password, _id, __v, ...others } = user._doc;
+  res.status(StatusCodes.OK).json(others);
+}
+
+export async function httpGetUsersStats(req, res) {}
+
+// Hasn't been used
+export const showCurrentUser = async (req, res) => {
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
+export const logOutUser = async (req, res) => {
   res.cookie("token", "", {
     path: "/",
     httpOnly: true,
@@ -68,9 +94,9 @@ export const loginOutUser = asyncHandler(async (req, res) => {
     secure: false,
   });
   return res.status(StatusCodes.OK).json({ msg: "Successfully logged out" });
-});
+};
 
-export const getCurrentUser = asyncHandler(async (req, res) => {
+export const getCurrentUser = async (req, res) => {
   const { userId } = req.user;
   const user = await User.findById(userId);
   if (!user) throw new notFoundError("Unable to get User");
@@ -80,16 +106,16 @@ export const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ id, name, email, image, contact, bio });
-});
+};
 
-export const checkUserStatus = asyncHandler(async (req, res) => {
+export const checkUserStatus = async (req, res) => {
   const token = await req.cookies.token;
   if (!token) res.json(false);
   const decode = jwt.verify(token, process.env.JWT_SEC);
   if (!decode) res.json(false);
   return res.json(true);
-});
-export const updateUserProfile = asyncHandler(async (req, res) => {
+};
+export const updateUser = async (req, res) => {
   const { userId } = req.user;
   const user = await User.findById(userId);
   if (!user) throw new notFoundError("Unable to get User");
@@ -110,9 +136,9 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
     contact: updatedUser.contact,
     bio: updatedUser.bio,
   });
-});
+};
 
-export const updateUserPassword = asyncHandler(async (req, res) => {
+export const updateUserPassword = async (req, res) => {
   const { userId } = req.user;
 
   const { oldPassword, newPassword } = req.body;
@@ -128,9 +154,9 @@ export const updateUserPassword = asyncHandler(async (req, res) => {
   user.password = newPassword;
   await user.save();
   res.status(StatusCodes.OK).json({ msg: "Password change successful" });
-});
+};
 
-export const forgotPassword = asyncHandler(async (req, res) => {
+export const forgotPassword = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
   if (!user) throw new notFoundError("Email doesn't exist");
@@ -181,9 +207,9 @@ export const forgotPassword = asyncHandler(async (req, res) => {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR);
     throw new Error("Email not sent, please try again");
   }
-});
+};
 
-export const resetPassword = asyncHandler(async (req, res) => {
+export const resetPassword = async (req, res) => {
   const { resetToken } = req.params;
   const { password, confirmPassword } = req.body;
 
@@ -206,4 +232,88 @@ export const resetPassword = asyncHandler(async (req, res) => {
   res.status(StatusCodes.OK).json({
     msg: "Password Reset Successful, Please Login",
   });
-});
+};
+
+// UPDATE USER ADDRESS
+// ADD Address and Payment Information
+export const httpAddAddress = async (req, res) => {
+  const { address, paymentInformation } = req.body;
+  const { userId } = req.user;
+  const user = await User.findById(userId).select("-password");
+  if (!user) throw new notFoundError("Login to Add Address");
+  if (!address || !paymentInformation)
+    throw new BadRequestError("Please provide address and payment Information");
+
+  const loc = await geocoder.geocode(address);
+  user.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+  };
+  // Do not save address
+  user.address = undefined;
+  await User.create({ loc, paymentInformation });
+
+  await user.save();
+
+  return res
+    .status(StatusCodes.CREATED)
+    .json({ msg: "Address successfully added" });
+  // }
+};
+
+// UPDATE USER ADDRESS
+export async function httpUpdateAddress(req, res) {
+  const { address } = req.body;
+  const { userId } = req.user;
+  const user = await User.findById(userId).select("-password");
+  if (!user) throw new notFoundError("Login to Add Address");
+
+  if (!address)
+    throw new BadRequestError("Please provide address and payment Information");
+  const loc = await geocoder.geocode(address);
+
+  const newAddress = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+  };
+  user.addresses.push(newAddress);
+  console.log("addresses: ", user.addresses.location);
+
+  // Do not save address
+  user.address = undefined;
+  // user.location = loc;
+
+  user.paymentInformation = paymentInformation;
+
+  const newAdd = user.addresses;
+
+  const updateOrder = await User.updateOne({ $push: { newAdd: newAddress } });
+
+  await user.save();
+  return res.status(StatusCodes.OK).json({ msg: "Address added successfully" });
+}
+
+// formattedAddress: loc[0].formattedAddress,
+// };
+// console.log("new Address: ", newAddress);
+// user.location.push(newAddress);
+// console.log("newLocation: ", user.location);
+
+// // Do not save address
+// user.address = undefined;
+// // const newAdd = user.addresses;
+// // const updateOrder = await User.updateOne({ $push: { newAdd: newAddress } });
+// // console.log("updatedAddress: ", updateOrder);
+// await user.save();
+// console.log("user: ", user);
+// res.status(StatusCodes.OK).json({ msg: "Address added successfully" });
+
+// throw new BadRequestError("Please provide address and payment Information");
+
+// // Do not save address
+// user.address = undefined;
+// // user.location = loc;
+// // console.log("LOC: ", loc);
+// user.paymentInformation = paymentInformation;

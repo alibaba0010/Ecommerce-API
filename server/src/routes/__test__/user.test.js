e9609aabe51fc1a9d83a3abb42ec7fa639bf1af5
alibaba0010 <zakariyahali100@gmail.com>
2023-08-11 10:59:54 +0100

    modifying test

--- server/src/routes/__test__/user.test.js
+++ server/src/routes/__test__/user.test.js
@@ -1,56 +1,113 @@
-import supertest from "supertest";
+import request from "supertest";
 import { app } from "../../app";
 
-// *******SIGNUP*****
-it("returns a 201 on successful signup", async () => {
+// *******SIGN UP AS USERS OR ADMIN *****
+it("returns a 201 on successful register", async () => {
   return request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
     .send({
+      username: "test",
       email: "test@test.com",
       password: "password",
+      confirmPassword: "password",
     })
     .expect(201);
-}, 10000);
+});
 
-it("returns a 400 with an invalid email", async () => {
+it("returns a 400 with an invalid username", async () => {
   return request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
     .send({
-      email: "alskdflaskjfd",
+      username: "test", //put invlid name
+      email: "test@test.com",
       password: "password",
+      confirmPassword: "password",
     })
     .expect(400);
-}, 10000);
-
+});
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
+    .send({
+      username: "test",
+      email: "alskdflaskjfd",
+      password: "password",
+      confirmPassword: "password",
+    })
+    .expect(400);
+});
 it("returns a 400 with an invalid password", async () => {
   return request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
+    .send({
+      username: "test",
+      email: "alskdflaskjfd",
+      password: "password",
+      email: "test@test.com",
+      confirmPassword: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an unable to confirm password", async () => {
+  return request(app)
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
     .send({
       email: "alskdflaskjfd",
       password: "p",
     })
     .expect(400);
-}, 10000);
+});
 
-it("returns a 400 with missing email or password", async () => {
+// ******MISSING VALUE
+it("returns a 400 with missing username, email, password or password", async () => {
   await request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
     .send({
       email: "test@test.com",
-    })
-    .expect(400);
-
-  await request(app)
-    .post("/api/v1/users/signup")
-    .send({
       password: "alskjdf",
+      confirmPassword: "alskjdf",
     })
     .expect(400);
-}, 10000);
+  await request(app)
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
+    .send({
+      username: "test",
+      password: "alskjdf",
+      confirmPassword: "alskjdf",
+    })
+    .expect(400);
+  await request(app)
+    .post("/v1/users/register")
+    .post("/v1/admin/register")
+    .send({
+      username: "test",
+      email: "test@test.com",
+      confirmPassword: "alskjdf",
+    })
+    .expect(400);
+});
+await request(app)
+  .post("/v1/users/register")
+  .post("/v1/admin/register")
+  .send({
+    username: "test",
+    email: "test@test.com",
+    password: "alskjdf",
+  })
+  .expect(400);
 
-it("sets a cookie after successful signup", async () => {
+// *****SET COOKIE AFTER LOGIN ****
+it("sets a cookie after successful login", async () => {
   const response = await request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/login")
     .send({
       email: "test@test.com",
       password: "password",
@@ -58,13 +115,13 @@ it("sets a cookie after successful signup", async () => {
     .expect(201);
 
   expect(response.get("Set-Cookie")).toBeDefined();
-}, 10000);
+});
 
 //**********LOGIN*************
 
 it("fails with 400 when a email that does not exist is supplied", async () => {
   await request(app)
-    .post("/api/v1/users/signin")
+    .post("/v1/users/login")
     .send({
       email: "test@test.com",
       password: "password",
@@ -74,7 +131,7 @@ it("fails with 400 when a email that does not exist is supplied", async () => {
 
 it("fails when an incorrect password is supplied", async () => {
   await request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
     .send({
       email: "test@test.com",
       password: "password",
@@ -82,7 +139,7 @@ it("fails when an incorrect password is supplied", async () => {
     .expect(201);
 
   await request(app)
-    .post("/api/v1/users/signin")
+    .post("/v1/users/login")
     .send({
       email: "test@test.com",
       password: "aslkdfjalskdfj",
@@ -92,7 +149,7 @@ it("fails when an incorrect password is supplied", async () => {
 
 it("responds with a cookie when given valid credentials", async () => {
   await request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
     .send({
       email: "test@test.com",
       password: "password",
@@ -100,7 +157,7 @@ it("responds with a cookie when given valid credentials", async () => {
     .expect(201);
 
   const response = await request(app)
-    .post("/api/v1/users/signin")
+    .post("/v1/users/login")
     .send({
       email: "test@test.com",
       password: "password",
@@ -111,9 +168,9 @@ it("responds with a cookie when given valid credentials", async () => {
 });
 
 //**********SIGN OUT*************
-it("clears the cookie after signing out", async () => {
+it("clears the cookie after loging out", async () => {
   await request(app)
-    .post("/api/v1/users/signup")
+    .post("/v1/users/register")
     .send({
       email: "test@test.com",
       password: "password",
@@ -121,7 +178,7 @@ it("clears the cookie after signing out", async () => {
     .expect(201);
 
   const response = await request(app)
-    .post("/api/v1/users/signout")
+    .post("/v1/users/signout")
     .send({})
     .expect(200);
 
@@ -132,10 +189,10 @@ it("clears the cookie after signing out", async () => {
 
 //**********CURRENT USER*************
 it("responds with details about the current user", async () => {
-  const cookie = await global.signin();
+  const cookie = await global.login();
 
   const response = await request(app)
-    .get("/api/users/currentuser")
+    .get("/users/currentuser")
     .set("Cookie", cookie)
     .send()
     .expect(200);
@@ -145,7 +202,7 @@ it("responds with details about the current user", async () => {
 
 it("responds with null if not authenticated", async () => {
   const response = await request(app)
-    .get("/api/v1/users/currentuser")
+    .get("/v1/users/currentuser")
     .send()
     .expect(200);
 
